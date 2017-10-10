429a58b51be37e6fbfcb51f4f82fa80be9e28cff
acarey2 <acarey@gcds.net>
2017-10-10 07:52:30 -0400

    First commit

---
+++ personality_survey.py
@@ -0,0 +1,11 @@
+print("What is your favorite animal?")
+animal = input()
+if animal == "cat":
+    print ("Woah, mine too!")       
+elif animal == "dog":
+    print("That's my second favorite")
+
+else:
+    print (animal  + " sounds cool.")
+
+        
