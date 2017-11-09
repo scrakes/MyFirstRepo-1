9b491965f06d64cc91b24fa33feec6d28f24c7b5
acarey2 <acarey@gcds.net>
2017-11-09 09:53:18 -0500

    Initial

---
+++ Mad Libs 2.py
@@ -0,0 +1,55 @@
+print ("write a person ")
+person1 = input ()
+print "write a football team ")
+footballteam1 = input ()
+print "write a adjective"
+adjective = input ()
+print "write a another person "
+person2 = input ()
+print "write a verb"
+verb2 = input ()
+print "write another football team "
+footballteam2 = input ()
+print "write a another person "
+person3 = input ()
+print "write a verb ending in ed"
+edverb = input ()
+print "write another adjective"
+adjective = input ()
+print "write a adverb"
+adverb = input ()
+
+print ("Sunday afternoon"  + person1 "Was a bench warmer on the  "
+       + footballteam1 + " and they were playing "
+       + footballteam2 + " it's the game of a century! "
+       + person1 + " wasn't expected to play in the game but their best WR "
+       + person2 + " was injured because "
+       + person3 + edverb " him down and he broke his leg. Now "
+       + person1 + " has to play in 4th quarter. "
+       + person1 + "was very "
+       + adjective +  "There was onyl 2 minutes left in the game when "
+       + person1 +  " had to go in. They were down 13 - 7. First Down. The center snaps the "
+       + noun + " to the quarterback and "
+       + person1 "ran down the sidelines. The quarterback "
+       + verb2 + " the "
+       + noun + "to "
+       + person1 " catched the ball and started to run down the sidelines. Unfortunatly he was mauled by "
+       + person4 "First down, said the ref. The crowd cheer and  "
+       + person1 " felt very "
+       + adjective2 + "and he played the rest of the game "
+       + adverb + "they lost but "
+       + person1 + "played a great game"
+
+    
+       
+    
+
+
+
+
+
+
+
+
+
+       )
---
+++ personality_AC.py
@@ -0,0 +1,31 @@
+name = "Annie"
+state = "New York"
+city = "Paris"
+book = "Maximum Ride"
+tvshow = "Friends"
+sport = "squash"
+friends = "Greer and Maddy and Ford"
+person = "Annie"
+print (name + " likes the visit " + state + " and " + city)
+print (name + " watches " + tvshow + " and reads " + book)
+print (friends + " thinks " + person + " is cool")
+
+print("What's your favorite color?")
+color = input().lower()
+
+if color == "purple":
+    print ("Purple is my favorite color")
+
+else :
+    print ( color + " is nice too")
+
+print ("What's your favorite band?")
+band = input().title()
+
+if band == "Queen":
+    print ("Queen is my favorite band too")
+
+else:
+    print ("I heard" + band + "is good too")
+    
+       
