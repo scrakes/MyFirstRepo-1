e26603273183cd5b1cebe4594fd183db891b12c1
scrakes <33459005+scrakes@users.noreply.github.com>
2017-11-14 11:04:10 -0500

    Update Mad Libs 2.py
    I had to fix a bunch of errors

--- Mad Libs 2.py
+++ Mad Libs 2.py
@@ -1,55 +1,38 @@
+import time
 print ("write a person ")
 person1 = input ()
-print "write a football team ")
+print ("write a football team ")
 footballteam1 = input ()
-print "write a adjective"
-adjective = input ()
-print "write a another person "
+print ("write a adjective")
+adjective1 = input ()
+print ("write a another person ")
 person2 = input ()
-print "write a verb"
+print ("write a noun")
+noun1 = input ()
+print ("write a verb")
 verb2 = input ()
-print "write another football team "
+print ("write another football team ")
 footballteam2 = input ()
-print "write a another person "
+print ("write a another person ")
 person3 = input ()
-print "write a verb ending in ed"
-edverb = input ()
-print "write another adjective"
-adjective = input ()
-print "write a adverb"
-adverb = input ()
+print ("write a verb ending in ed")
+edverb1 = input ()
+print ("write another adjective")
+adjective2 = input ()
+print ("write a adverb")
+adverb1 = input ()
 
-print ("Sunday afternoon"  + person1 "Was a bench warmer on the  "
-       + footballteam1 + " and they were playing "
-       + footballteam2 + " it's the game of a century! "
-       + person1 + " wasn't expected to play in the game but their best WR "
-       + person2 + " was injured because "
-       + person3 + edverb " him down and he broke his leg. Now "
-       + person1 + " has to play in 4th quarter. "
-       + person1 + "was very "
-       + adjective +  "There was onyl 2 minutes left in the game when "
-       + person1 +  " had to go in. They were down 13 - 7. First Down. The center snaps the "
-       + noun + " to the quarterback and "
-       + person1 "ran down the sidelines. The quarterback "
-       + verb2 + " the "
-       + noun + "to "
-       + person1 " catched the ball and started to run down the sidelines. Unfortunatly he was mauled by "
-       + person4 "First down, said the ref. The crowd cheer and  "
-       + person1 " felt very "
-       + adjective2 + "and he played the rest of the game "
-       + adverb + "they lost but "
-       + person1 + "played a great game"
+print("Sunday afternoon"  + person1 + "Was a bench warmer on the " + footballteam1 + " and they were playing " + footballteam2 + " it's the game of a century! ")
+print(person1 + " wasn't expected to play in the game but their best WR " + person2 + " was injured because " + person3 + edverb1 + " him down and he broke his leg.")
+print("Now " + person1 + " has to play in 4th quarter. ")
+print(person1 + "was very " + adjective1 +  "There was onyl 2 minutes left in the game when " + person1 +  " had to go in. They were down 13 - 7. First Down.")
+print("The center snaps the " + noun1 + " to the quarterback and " + person1 + "ran down the sidelines.")
+print("The quarterback " + verb2 + " the " + noun + "to " + person1 + " catched the ball and started to run down the sidelines.")
+print("Unfortunatly he was mauled by " + person4 + "First down, said the ref.")
+print("The crowd cheer and  " + person1 + " felt very " + adjective2 + "and he played the rest of the game " + adverb1 + "they lost but " + person1 + "played a great game.")
 
     
        
     
 
-
-
-
-
-
-
-
-
-       )
+time.sleep(200)
